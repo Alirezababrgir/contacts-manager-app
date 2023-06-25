3b9bb3cd856c5e66c787f935ef22719cb3378433
Alirezababrgir <zakhargithub021@gmail.com>
2023-06-25 13:34:28 -0400

    add dracula color pallet and import in navbar

--- src/components/contact/searchbox.jsx
+++ src/components/contact/searchbox.jsx
@@ -1,8 +1,9 @@
+import { Purple,Background,comment } from "../../helpers/color";
 const Sbox = () => {
     return (
         <form className="d-flex" role="search" dir="ltr">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <input className="form-control me-2" type="search" placeholder="جستجوی مخطب " aria-label="Search" />
+            <button className="btn btn-primary" type="submit"><i class="bi bi-search"></i></button>
         </form>
     )
 }
