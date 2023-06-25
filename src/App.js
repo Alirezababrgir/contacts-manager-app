556c63d60206186bdb9de14d022d441332a6e3d0
Alirezababrgir <zakhargithub021@gmail.com>
2023-06-24 20:51:45 -0400

    download vazir font , add CDN bootstrap bandle and bootstrap icon -49

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>لیست مخاطبین</h1>
+        <div className='btn btn-primary'>
+        <i class="bi bi-windows"></i>
+        windows
+        </div>
+        
       </header>
     </div>
   );
