2418ae1a7ee4e46a11b1c4f7d6a559b495ebdf99
Alirezababrgir <zakhargithub021@gmail.com>
2023-07-12 20:39:56 -0400

    not defind

--- src/App.js
+++ src/App.js
@@ -2,30 +2,50 @@ import './App.css';
 import Contacts from './components/contact/contacts';
 import Navbar from './components/navbar';
 import Addcon from './components/contact/addcontact';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Viewcon from './components/contact/viewcontact'
 import Editcon from './components/contact/editcontact'
-import { Route,Routes,Navigate } from 'react-router-dom';
-
+import { Route, Routes, Navigate } from 'react-router-dom';
+import axios from 'axios';
 
 //import { Fragment } from 'react';
 function App() {
-  const [getstate,setstate]=useState([]);
-  const [getloader,setloader]=useState([false]);
+  const [getstate, setstate] = useState([]);
+  const [getgroup, setgroup] = useState([]);
+  const [getloader, setloader] = useState([false]);
+
+  useEffect(() => {
+    async function fetchdata() {
+      try{
+        setloader(true);
+        const {data:contactdat}=await axios.get('http://localhost:9000/contacts');
+        const {data:groupdata}=await axios.get('http://localhost:9000/groups');
+        console.log(groupdata)
+        getstate(contactdat);
+        getgroup(groupdata);
+        setloader(false);
+      }catch(err){
+        console.log(err.message)
+
+      }
+
+    }
+    fetchdata();
+  }, [])
   return (
     <div className='App'>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Navigate to={"/contacts"}/>}/>
-          <Route path='/contacts' element={<Contacts state={getstate} getloader={getloader}/>}/>
-          <Route path='/contacts/add' element={<Addcon/>}/>
-          <Route path='/contacts/:contactID' element={<Viewcon/>}/>
-          <Route path='/contacts/edit/:contactID' element={<Editcon/>}/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to={"/contacts"} />} />
+        <Route path='/contacts' element={<Contacts state={getstate} getloader={getloader} />} />
+        <Route path='/contacts/add' element={<Addcon />} />
+        <Route path='/contacts/:contactID' element={<Viewcon />} />
+        <Route path='/contacts/edit/:contactID' element={<Editcon />} />
 
-        </Routes>
+      </Routes>
     </div>
 
-   
+
   );
 }
 export default App;
